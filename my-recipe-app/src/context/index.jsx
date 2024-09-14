a6c9b3e2d2cfae6c2ceacd8f6a2b8479d801f350
jessicayin66 <109621835+jessicayin66@users.noreply.github.com>
2024-09-14 15:04:26 -0400

    Completed tutorial

--- my-recipe-app/src/context/index.jsx
+++ my-recipe-app/src/context/index.jsx
@@ -1,13 +1,68 @@
-import { createContext, useState} from "react";
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const GlobalContext = createContext(null);
 
-export default function GlobalState({children}) {
-    const [searchParam, setSearchParam] = useState("");
+export default function GlobalState({ children }) {
+  const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+  const [recipeDetailsData, setRecipeDetailsData] = useState(null);
+  const [favouritesList, setFavouritesList] = useState([]);
 
-    return (
-        <GlobalContext.Provider value={{ searchParam, setSearchParam }}>
-            {children}
-        </GlobalContext.Provider>
-    );
+  const navigate = useNavigate();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+      );
+      const data = await res.json();
+      if (data?.data?.recipes) {
+        setRecipeList(data?.data?.recipes);
+        setLoading(false);
+        setSearchParam("");
+        navigate('/');
+      }
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+      setSearchParam("");
+    }
+  }
+
+  function handleAddToFavourite(getCurrentItem) {
+    console.log(getCurrentItem);
+    let copyFavouritesList = [...favouritesList];
+    const index = copyFavouritesList.findIndex(item => item.id === getCurrentItem.id);
+
+    if (index === -1) {
+      copyFavouritesList.push(getCurrentItem);
+    } else {
+      copyFavouritesList.splice(index, 1);
+    }
+
+    setFavouritesList(copyFavouritesList);
+  }
+
+  console.log(favouritesList, recipeList);
+
+  return (
+    <GlobalContext.Provider
+      value={{ 
+        searchParam, 
+        loading, 
+        recipeList, 
+        setSearchParam, 
+        handleSubmit, 
+        recipeDetailsData, 
+        setRecipeDetailsData, 
+        handleAddToFavourite, 
+        favouritesList 
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
--- my-recipe-app/src/pages/favourites/index.jsx
+++ my-recipe-app/src/pages/favourites/index.jsx
@@ -1,3 +1,19 @@
+import RecipeItem from "../../components/recipe-item";
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
+
 export default function Favourites() {
-    return <div>Favourites</div>
+    const {favouritesList  } = useContext(GlobalContext);
+
+    return (<div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+        {
+            favouritesList && favouritesList.length > 0  
+            ? favouritesList.map((item) => <RecipeItem item={item} />)
+            : <div>
+                <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+                    Nothing has been added to Favourites.</p>
+            </div>
+        }
+        </div>
+    );
 }
